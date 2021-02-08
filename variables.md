faa61e0ab6baad3fdbf0965a8f5ccd5fd6668af6
hossam-khalaf <hossamhassan303089@gmail.com>
2021-02-08 06:08:03 +0200

    variables

---
+++ variables.md
@@ -0,0 +1,9 @@
+# VARIABLES
+
+## DRY => Don't Repeat Yourself
+
+variable is a box storing a piece of data stored in memory
+
+## Data types
+
+- string
---
+++ variables.py
@@ -0,0 +1,118 @@
+          
+# first_name = 'Heba'
+# last_name = 'Ali'
+# person_age = 26
+
+## = is not Equal , it's Assignment operator
+
+# \ back slash is called: escaping characters
+
+# print(f'I\'m {first_name.upper()} {last_name.lower()} \t and my age is {person_age}')
+
+# print(type(person_age)) 
+
+# len() for length
+# print(len(first_name), len(last_name))
+
+# Bracket notation && indexing
+# print(first_name[-1])
+
+# summary = f'{first_name}  {last_name}  {str(person_age)}'
+
+# print(summary)
+
+# sentence = 'hello from the other world!'
+
+
+# print(sentence.replace(sentence[0:5], 'hi'))
+
+# Numbers
+# x = 2
+
+# y = 7
+
+# h = 10
+
+# z = x / (y + h)
+
+# print(getsizeof(z))
+
+
+# MAD LIBS game
+# num = 5 + 5
+# fruit = input('choose a fruit: ')
+# color = input('choose a color: ')
+# verb = input('choose a verb: ')
+# verb2 = input('choose another verb: ')
+# adj = input('choose a adj: ')
+# noun = input('choose a noun: ')
+# noun2 = input('choose another noun: ')
+
+# print(f'The {noun} is {5 + 5} {color}\n I {verb} {fruit}\n my {noun2} {verb2} {adj}')
+
+# device = 'computer'
+# device = 'Mobile'
+# print(device.lower())
+
+
+# first_name, last_name = 'Heba', 'Ali'
+
+# print(first_name, last_name)
+
+# constant
+
+# ESMO_EH = 13
+# ESMO_EH = 10
+# print(ESMO_EH)
+
+
+
+# booleans
+
+# # is_married = True
+# first_name = 'hossam'
+
+# print(first_name.isupper())
+
+
+# # 1- get the first number
+# num1 = input('type the first number: ')
+# # 2- get the other number
+
+# num2 = input('type the other number: ')
+# # store result of (first + other) in variable
+# result = float(num1) + float(num2)
+# # print the result
+
+# print(result)
+
+
+# Calculator
+
+# num1 = input('type the first number: ')
+
+# operation = input('choose operation:')
+
+# num2 = input('type the other number: ')
+
+
+# if operation == '+':
+#   print(round(float(num1) + float(num2), 1))
+
+# elif operation == '-':
+#   print(float(num1) - float(num2))
+
+# elif operation == '/':
+#   print(float(num1) / float(num2))
+
+# elif operation == '*':
+#   print(float(num1) * float(num2))
+
+# else:
+#    print('please try again , it is a calculator not a magic box!')
+
+
+# type
+# job = 'dentist'
+# num = False
+# print(type(num))
