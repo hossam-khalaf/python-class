732ecf0122116f3808d004f47db976f05e867fea
hossam-khalaf <hossamhassan303089@gmail.com>
2021-03-09 02:45:11 +0200

    vars

---
+++ var2.py
@@ -0,0 +1,31 @@
+# comment    single line comment
+
+'''
+multible line comment
+'''
+
+"""
+multible line comment
+"""
+
+
+# x = 1  # int
+# y = 3.87  # float
+# name = 'Hamada'  # string
+# is_tall = True # boolean 
+
+# multiple assignment
+
+x, y, name, is_tall = (1, 3.87, 'Hamada', True)
+
+# # basic Math
+# z = x + y
+# print(z)
+
+# Casting => convert from type to type
+# x = str(x)
+# x = float(x)
+# x = '3'
+# x = int(x)
+
+# print(type(x), x)
