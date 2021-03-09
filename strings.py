881f5390c44d804621fcb0ce68110be422317dec
hossam-khalaf <hossamhassan303089@gmail.com>
2021-03-09 20:04:37 +0200

    strings

---
+++ strings.py
@@ -0,0 +1,53 @@
+# Strings surrownded by single quotes or double quotes
+
+# name = 'Heba'
+
+# age = 18
+
+# Concatenate
+# print('My Name Is ' + name + ' And My Age is ' + str(age) + ' years old!')
+
+# String Formating
+
+# print('My Name Is {name} And My Age is {age} years old!'.format(name = name, age = age))
+# F- Strings
+# print(f'My Name Is {name} And My Age is {age} years old!')
+
+# string methods
+
+phrase = 'hello world'
+
+# # capitalize
+# print(phrase.capitalize())
+
+# # uppercase
+# print(phrase.upper())
+
+# # lowercase
+# print(phrase.lower())
+
+# # swapcase
+# print(phrase.swapcase())
+
+# length
+# print(len(phrase))
+
+# # replace
+# print(phrase.replace('world', 'Heba'))
+
+# count
+# substring = 'h'
+# print(phrase.count(substring))
+
+# start with
+# print(phrase.startswith('hell'))
+
+# end with
+# print(phrase.endswith('ld'))
+
+#split
+# print(phrase.split())
+
+#find 
+# print(phrase.find('l'))
+
